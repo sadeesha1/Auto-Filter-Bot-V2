cffd212dd79066709940c255b36d2763c793d029
sadeesha1 <87900251+sadeesha1@users.noreply.github.com>
2021-12-04 19:42:05 +0530

    Update config.py

--- config.py
+++ config.py
@@ -5,31 +5,31 @@ from logging.handlers import RotatingFileHandler
 
 
 # Get a bot token from botfather
-TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "")
+TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "2146361275:AAF3mgx16FEFiw8ug6IHyXoCZlsrMKyu-e0")
 
 # Get from my.telegram.org (or @UseTGXBot)
-APP_ID = int(os.environ.get("APP_ID", ""))
+APP_ID = int(os.environ.get("APP_ID", "9956617"))
 
 # Get from my.telegram.org (or @UseTGXBot)
-API_HASH = os.environ.get("API_HASH", "")
+API_HASH = os.environ.get("API_HASH", "8f6e046c7e7f35808ae11e77f1ecc890")
 
 # Generate a user session string 
-TG_USER_SESSION = os.environ.get("TG_USER_SESSION", "")
+TG_USER_SESSION = os.environ.get("TG_USER_SESSION", "AQBZidX6xpS7DlheXbgjAgjnTfDl9jWSdOtTp-F0DZKdD-FEs0lkCwcNhvb3S5VvwaoV47yqoq5JGux4SwYcb577h3PrPK4Wjwrgktq9l4dtBKWCF8_25SOtHpCz2RicN6KV3B_66ZcvuL_IH6TEfBmXDWNb_IZfsAa1OYCSh_s8A8OQiMjnBhX6UwPiwfeZRMweeAul-rdspd-uvXSTxAarwSB3kTXdPbFkBejvNwqWC6iYT2V9EdyX51S9bVxm5HGGVk6LCTkNNj1fMNb5es7pYOaxFBcsLWbzZ1xC-V7UCixFPRRifaDVbLHOFo-GXZrJg_9nvPrY53xB5aVHT70oe6AmBgA")
 
 # Database URL from https://cloud.mongodb.com/
-DATABASE_URI = os.environ.get("DATABASE_URI", "")
+DATABASE_URI = os.environ.get("DATABASE_URI", "mongodb+srv://erichdaniken:erichdaniken@cluster0.7enua.mongodb.net/myFirstDatabase?retryWrites=true&w=majority")
 
 # Your database name from mongoDB
-DATABASE_NAME = os.environ.get("DATABASE_NAME", "")
+DATABASE_NAME = os.environ.get("DATABASE_NAME", "Cluster0")
 
 # ID of users that can use the bot commands
-AUTH_USERS = set(int(x) for x in os.environ.get("AUTH_USERS", "").split())
+AUTH_USERS = set(int(x) for x in os.environ.get("AUTH_USERS", "2074093062").split())
 
 # Should bot search for document files in channels
 DOC_SEARCH = os.environ.get("DOC_SEARCH", "yes").lower()
 
 # Should bot search for video files in channels
-VID_SEARCH = os.environ.get("VID_SEARCH", "no").lower()
+VID_SEARCH = os.environ.get("VID_SEARCH", "yes").lower()
 
 # Should bot search for music files in channels
 MUSIC_SEARCH = os.environ.get("MUSIC_SEARCH", "no").lower()
